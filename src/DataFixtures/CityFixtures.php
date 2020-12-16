4ff9877698215472c86c5d4c974190bb3a4ef0dc
ClaireV38 <claireverdier00@gmail.com>
2020-12-16 11:46:38 +0100

    \#38 creation of CityFixtures

---
+++ src/DataFixtures/CityFixtures.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+use App\Entity\City;
+
+class CityFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker  =  Faker\Factory::create('fr_FR');
+        for ($i = 1; $i <= 50; $i++) {
+            $city = new City();
+            $city->setName($faker->city());
+            $city->setZipcode(intval($faker->postcode()));
+            $manager->persist($city);
+            $this->addReference('city_' . $i, $city);
+        }
+
+        $manager->flush();
+    }
+}
