52a8191202a80c0d0f6a21b8b2b90c5f5959e9ad
ClaireV38 <claireverdier00@gmail.com>
2020-12-17 09:01:13 +0100

    #38 generate fixtures for applicant

---
+++ src/DataFixtures/ApplicantFixtures.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+use App\Entity\Applicant;
+
+class ApplicantFixtures extends Fixture implements DependentFixtureInterface
+{
+    /**
+     * nb objects to create
+     * @var int
+     **/
+    private const NB_OBJECT = 50;
+
+    public function getDependencies()
+    {
+        return [UserFixtures::class, CityFixtures::class];
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $faker  =  Faker\Factory::create('fr_FR');
+        for ($i = 1; $i <= self::NB_OBJECT; $i++) {
+            $applicant = new Applicant();
+            $applicant->setUser($this->getReference('appl_user_' . $i));
+            $applicant->setCity($this->getReference('city_' . rand(1, self::NB_OBJECT)));
+            $applicant->setFirstname($faker->firstName());
+            $applicant->setLastname($faker->lastName());
+            $applicant->setPersonality($faker->text(100));
+            $applicant->setMobility($faker->text(100));
+            $manager->persist($applicant);
+            $this->addReference('applicant_' . $i, $applicant);
+        }
+        $manager->flush();
+    }
+}
