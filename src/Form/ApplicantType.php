9a0683a517365e3883f33e84ecfec97afdb3e4ec
Thomas Leroy <thomasleroy50@gmail.com>
2021-01-02 10:16:12 +0100

    create media query desktop for ApplicantNewForm

--- src/Form/ApplicantType.php
+++ src/Form/ApplicantType.php
@@ -10,12 +10,16 @@ use Doctrine\ORM\EntityRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\DomCrawler\Field\TextareaFormField;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\ChoiceList\ChoiceList;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
 
 class ApplicantType extends AbstractType
 {
@@ -29,14 +33,15 @@ class ApplicantType extends AbstractType
             ->add('firstname', TextType::class)
             ->add('lastname')
             ->add('personality')
-            ->add('mobility')
-            ->add('skills', EntityType::class, [
-                'class' => Skill::class,
+            ->add('mobility', TextType::class)
+            ->add('skills', null, [
 //              'expanded' => true,
-                'multiple' => true,
-                'query_builder' => function (EntityRepository $er) {
-                    return $er->createQueryBuilder('s');
-                },
+//              'multiple' => true,
+                'label' => false,
+//                'query_builder' => function (EntityRepository $er) {
+//                    return $er->createQueryBuilder('s')
+//                        ;
+//                },
 //              'choices' => $builder->getData()->getSkillCategory(),
                 'choice_label' => 'name',
             ])
