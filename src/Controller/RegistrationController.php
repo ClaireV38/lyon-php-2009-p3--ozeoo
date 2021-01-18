1ef27e45a54de67c3c0c3c658378c137f880b03f
Rainbow-of-Dawn <as.linkpro@gmail.com>
2021-01-18 12:34:24 +0100

    test

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use App\Security\EmailVerifier;
 use App\Entity\Applicant;
 use App\Entity\Company;
 use App\Entity\User;
@@ -11,14 +13,22 @@ use App\Security\LoginFormAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 class RegistrationController extends AbstractController
 {
+    private $emailVerifier;
+
+    public function __construct(EmailVerifier $emailVerifier)
+    {
+        $this->emailVerifier = $emailVerifier;
+    }
+
     /**
      * @Route("/register/company", name="app_register_company")
      * @return Response
@@ -27,8 +37,7 @@ class RegistrationController extends AbstractController
         Request $request,
         UserPasswordEncoderInterface $passwordEncoder,
         GuardAuthenticatorHandler $guardHandler,
-        LoginFormAuthenticator $authenticator,
-        MailerInterface $mailer
+        LoginFormAuthenticator $authenticator
     ): ?Response {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -48,12 +57,16 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
-            $email = (new Email())
-                ->from($this->getParameter('mailer_from'))
-                ->to($user->getEmail())
-                ->subject('Prise en compte de votre inscription')
-                ->html($this->renderView('company/newCompanyEmail.html.twig'));
-                $mailer->send($email);
+
+            // generate a signed url and email it to the user
+            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+                (new TemplatedEmail())
+                    ->from(new Address('your_email@example.com', 'Oze La Diversite'))
+                    ->to($user->getEmail())
+                    ->subject('Please Confirm your Email')
+                    ->htmlTemplate('company/newCompanyEmail.html.twig')
+            );
+            // do anything else you need here, like send an email
 
             return $guardHandler->authenticateUserAndHandleSuccess(
                 $user,
@@ -76,8 +89,7 @@ class RegistrationController extends AbstractController
         Request $request,
         UserPasswordEncoderInterface $passwordEncoder,
         GuardAuthenticatorHandler $guardHandler,
-        LoginFormAuthenticator $authenticator,
-        MailerInterface $mailer
+        LoginFormAuthenticator $authenticator
     ): ?Response {
         $user = new User();
         $applicant = new Applicant();
@@ -99,12 +111,16 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
-            $email = (new Email())
-                ->from($this->getParameter('mailer_from'))
-                ->to($user->getEmail())
-                ->subject('Confirmation de création de compte')
-                ->html($this->renderView('applicant/newApplicantEmail.html.twig'));
-            $mailer->send($email);
+
+            // generate a signed url and email it to the user
+            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+                (new TemplatedEmail())
+                    ->from(new Address('your_email@example.com', 'Oze La Diversite'))
+                    ->to($user->getEmail())
+                    ->subject('Please Confirm your Email')
+                    ->htmlTemplate('applicant/newApplicantEmail.html.twig')
+            );
+            // do anything else you need here, like send an email
 
             return $guardHandler->authenticateUserAndHandleSuccess(
                 $user,
@@ -118,4 +134,27 @@ class RegistrationController extends AbstractController
             'ApplicantRegister' => $form->createView(),
         ]);
     }
+
+    /**
+     * @Route("/verify/email", name="app_verify_email")
+     */
+    public function verifyUserEmail(Request $request): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        // validate email confirmation link, sets User::isVerified=true and persists
+        try {
+            $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
+        } catch (VerifyEmailExceptionInterface $exception) {
+            $this->addFlash('verify_email_error', $exception->getReason());
+
+            return $this->redirectToRoute('app_register');
+        }
+
+        // @TODO Change the redirect on success and handle or remove the flash message in your templates
+        $this->addFlash('success', 'Your email address has been verified.');
+
+        return $this->redirectToRoute('app_register');
+    }
 }
+
