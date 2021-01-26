34f9bccb5eeaf6f7a95034c1b761a6f78f4c4163
Rainbow-of-Dawn <as.linkpro@gmail.com>
2021-01-26 11:54:59 +0100

    fixed the commit message

--- src/Entity/ResetPasswordRequest.php
+++ src/Entity/ResetPasswordRequest.php
@@ -26,11 +26,11 @@ class ResetPasswordRequest implements ResetPasswordRequestInterface
     /**
      * @ORM\ManyToOne(targetEntity=User::class)
      * @ORM\JoinColumn(nullable=false)
-     * @var User
+     * @var object|User
      */
     private $user;
 
-    public function __construct(User $user, \DateTimeInterface $expiresAt, string $selector, string $hashedToken)
+    public function __construct(object $user, \DateTimeInterface $expiresAt, string $selector, string $hashedToken)
     {
         $this->user = $user;
         $this->initialize($expiresAt, $selector, $hashedToken);
